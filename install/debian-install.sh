5996e834f5b4629bc59d35bb107d87cceded6ace
Alvaro Fimpel <afimpel@gmail.com>
2025-02-27 01:12:13 -0300

    fix: :fire: debian

--- install/debian-install.sh
+++ install/debian-install.sh
@@ -14,15 +14,8 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
-$STD apt-get install -y mc
+$STD apt-get install -y curl sudo mc
 msg_ok "Installed Dependencies"
 
 motd_ssh
 customize
-
-msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
-msg_ok "Cleaned"
